fb832a9943c32f9bddb4147754853afb673df44e
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-06-18 02:01:05 +0300

    taskflow project init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+  <div>
+    <NuxtPage />
+    <UNotifications />
+    <SubscribeModal />
+  </div>
+</template>
+
+<style>
+  body {
+  	@apply bg-white dark:bg-slate-950
+  }
+</style>
